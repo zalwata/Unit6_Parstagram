406b06d71735df7c7bb4e7fc92acf1fcd38c5a8a
aryumjeon <zalwata@hotmail.com>
2021-03-12 19:45:38 -0800

    Initial Commit

---
+++ Unit5_ParstagramTests/Unit5_ParstagramTests.swift
@@ -0,0 +1,33 @@
+//
+//  Unit5_ParstagramTests.swift
+//  Unit5_ParstagramTests
+//
+//  Created by Aryum Jeon on 3/12/21.
+//
+
+import XCTest
+@testable import Unit5_Parstagram
+
+class Unit5_ParstagramTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
