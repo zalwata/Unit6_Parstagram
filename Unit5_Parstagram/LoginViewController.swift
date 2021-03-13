c9c97dad3fbe6f869167d13ac861d814aff627ef
aryumjeon <zalwata@hotmail.com>
2021-03-12 20:38:03 -0800

    part5.3 is completed - 5.4 is required

---
+++ Unit5_Parstagram/LoginViewController.swift
@@ -0,0 +1,72 @@
+//
+//  LoginViewController.swift
+//  Unit5_Parstagram
+//
+//  Created by Aryum Jeon on 3/12/21.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password)
+        { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSeque", sender: nil)
+            } else
+            {
+                print("Error: \(error?.localizedDescription)")
+
+            }
+            
+        }
+        
+    }
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+        var user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground{(success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSeque", sender: nil)
+            } else
+            {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
